b10893ca9ac9cf0a4a0accdfdc52207b40279ca8
Keyon-Jerome <keyonjerome@gmail.com>
2020-06-06 22:17:21 -0400

    Added API integration + loading.

--- lib/rocketlist.dart
+++ lib/rocketlist.dart
@@ -1,9 +1,23 @@
 
+import 'package:SpaceLaunchNews/LaunchRequestAPI.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-class RocketListPage extends StatelessWidget{ 
+
+class RocketInfo {
+  final String title;
+  final String description;
+  final Image thumbnail;
+
+  RocketInfo(this.title, this.description, this.thumbnail);
+}
+class RocketListPage extends StatelessWidget{
+    Future<List> futureLaunch; 
   Size deviceSize;List<bool> numberTruthList = [true, true, true, true , true, true];
+
+  List<RocketInfo> rocketList = [
+    RocketInfo('Falcon 9 ','dasd',Image.asset('asasd'))
+  ];
 
 // Widget rocketList(BuildContext context) =>  {
   
@@ -12,55 +26,26 @@ class RocketListPage extends StatelessWidget{
 Widget appBarColumn(BuildContext context) => SafeArea(
       child:new Column(children:[Center(child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0), child: Text('Rocket List', textScaleFactor: 1.5, style: TextStyle(color: Colors.white),),))])      
-        // child: Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-        //   child: new Column(
-        //     children: <Widget>[
-        //       new Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //         children: <Widget>[
-        //           new IconButton(
-        //             icon: new Icon(
-        //               defaultTargetPlatform == TargetPlatform.android
-        //                   ? Icons.arrow_back
-        //                   : Icons.arrow_back_ios,
-        //               color: Colors.white,
-        //             ),
-        //             onPressed: () => Navigator.canPop(context)
-        //                 ? Navigator.pop(context)
-        //                 : null,
-        //           ),
-        //           new Center(child: new Text('Rocket List',textAlign: TextAlign.center,)
-        //           // new ProfileTile(
-        //           //   title: "Rocket List",
-        //           //   subtitle: "",
-        //           //   textColor: Colors.white,
-        //           // ),
 
-        //           ),],
-        //       ),
-        //     ],
-        //   ),
-        // ),
       );
 
-    Widget allCards(BuildContext context) => SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            appBarColumn(context),
+    // Widget allCards(BuildContext context) => SingleChildScrollView(
+    //     child: Column(
+    //       children: <Widget>[
+    //         appBarColumn(context),
 
-            SizedBox(
-              height: deviceSize.height * 0.01,
-            ),
-           Expanded(
-          child: SizedBox(
-            height: 200.0,
-            child:rocketList(context))),
-            // balanceCard(),
+    //         SizedBox(
+    //           height: deviceSize.height * 0.01,
+    //         ),
+    //        Expanded(
+    //       child: SizedBox(
+    //         height: 200.0,
+    //         child:rocketList(context))),
+    //         // balanceCard(),
 
-          ],
-        ),
-      );
+    //       ],
+    //     ),
+    //   );
  Widget _buildItemsForListView(BuildContext context, int index) {
       return Text('HI');
       // return ListTile(
@@ -69,63 +54,71 @@ Widget appBarColumn(BuildContext context) => SafeArea(
       //   // subtitle: Text(_newsArticles[index].title, style: TextStyle(fontSize: 18)),
       // );
   }
-Widget rocketList(BuildContext context) => 
-  ListView.builder(
-            itemCount: 6,
-              itemBuilder: (context, i){
-                if(numberTruthList[i]){
-                  return ListTile(
-                    title: Text("$i"),
-                  );
-                }
-              },
-  );
-    
+
   @override
   Widget build(BuildContext context) {
+    futureLaunch = fetchLaunch();
   deviceSize = MediaQuery.of(context).size;
     
       return Stack(
       // fit: StackFit.expand,
-      children:[Image.asset(
-            'assets/rocket.JPG',
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-      Scaffold(backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: Text('Rocket List',textAlign: TextAlign.center,style:TextStyle(fontFamily: 'Roboto'))
+      children:[
+        // Image.asset(
+        //     'assets/rocket.JPG',
+        //     height: MediaQuery.of(context).size.height,
+        //     width: MediaQuery.of(context).size.width,
+        //     fit: BoxFit.cover,
+        //   ),
+      Scaffold(backgroundColor: Color.fromRGBO(0,7, 45, 1.0),
+        appBar: AppBar( backgroundColor: Color.fromRGBO(0,7, 45, 1.0),
+          title: Text('Rocket List',textAlign: TextAlign.center,style:TextStyle(fontFamily: 'Roboto')),
         ),
-        body: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-           return     Card(margin: EdgeInsets.all(4),
-      child: ListTile(
-        leading: FlutterLogo(size: 56.0),
-        title: Text('Two-line ListTile'),
-        subtitle: Text('Here is a second line'),
-        // onTap: ,
+    //     body: ListView.builder(
+    //       itemCount: 5,
+    //       itemBuilder: (context, index) {
+    //        return     Card(margin: EdgeInsets.all(4),
+    //             child: ListTile(
+    //               leading: FlutterLogo(size: 56.0),
+    //               title: Text('Two-line ListTile'),
+    //               subtitle: Text('Here is a second line'),
+    //               onTap: () {
+
+    //               },
+    //     // onTap: ,
        
-      ),
-    );
-          }
-            
-          
-        )
+    //   ),
+    // );
+    //       }  
+    //     )
+    body:              FutureBuilder<List>(
+                future: futureLaunch,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+                    return     Card(margin: EdgeInsets.all(4),
+                          child: ListTile(
+                            leading: FlutterLogo(size: 56.0),
+                            title: Text(snapshot.data[index]['name']),
+                            subtitle: Text(snapshot.data[index]['id'].toString()),
+                            onTap: () {
+
+                            })
+                            );
+                  }
+                  );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  // By default, show a loading spinner.
+                  print(snapshot);
+                  return Center(child:CircularProgressIndicator());
+                },
+              ),
       )]);
-    return Scaffold(
-      // body: Stack(
-      //   fit: StackFit.expand,
-      //   children: <Widget>[
-      //     LoginBackground(
-      //       showIcon: false,
-      //     ),
-          
-      //      allCards(context)
-      //   ],
-      // ),
-    );
+
+    
   }
   
   }
