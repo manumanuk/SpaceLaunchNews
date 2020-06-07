fbbf2061c48db5131c1249abe29903da35b9dec3
aryankalia <kalia.aryan2013@gmail.com>
2020-06-07 13:21:13 -0400

    New name

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -35,11 +35,11 @@ class _HomePageState extends State<HomePageWidget> {
                 color: Colors.transparent,
                 shadowColor: Colors.black,
                 //margin: EdgeInsets.only(left: context.size.height/100*7, right: context.size.height/100*7, top: context.size.height/100*25, bottom: context.size.height/100*25),
-                margin: EdgeInsets.only(left: media.width/100*5, right: media.width/100*5, top: media.height/100*33, bottom: media.height/100*9), 
+                margin: EdgeInsets.only(left: media.width/100*12, right: media.width/100*12, top: media.height/100*35, bottom: media.height/100*9), 
                 child: Padding(
-                  padding: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(13),
                   child: Center(child: Text(
-                    'Space Launch News',
+                    'LiveLaunch',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Colors.white,
